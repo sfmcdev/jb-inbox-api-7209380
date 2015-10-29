ade8e9bd675e95ef1e8634476e0b897eea41c266
VCheng <vcheng@vcheng-ltr1.internal.salesforce.com>
2015-10-29 15:58:48 +1100

    updated ui to include UAT

--- public/ixn/activities/inbox/js/Inbox.js
+++ public/ixn/activities/inbox/js/Inbox.js
@@ -14,7 +14,8 @@ define( function( require ) {
     $(window).ready(onRender);
 
     connection.on('initActivity', function(payload) {
-        var titleEn;
+        var apiUrl;
+		var titleEn;
 		var contentEn;
 		var titleTc;
 		var contentTc;
@@ -33,6 +34,7 @@ define( function( require ) {
 				}
 			}
 			//oArgs.priority will contain a value if this activity has already been configured:
+			apiUrl = oArgs.apiUrl || toJbPayload['configurationArguments'].defaults.apiUrl;
 			titleEn = oArgs.titleEn || toJbPayload['configurationArguments'].defaults.titleEn;
 			contentEn = oArgs.contentEn || toJbPayload['configurationArguments'].defaults.contentEn;
 			titleTc = oArgs.titleTc || toJbPayload['configurationArguments'].defaults.titleTc;
@@ -44,11 +46,12 @@ define( function( require ) {
 			$('#version').html('Version: ' + data.version);
 		});                
 
-        $('#titleEn').val(titleEn);
+		$('#titleEn').val(titleEn);
 		$('#titleTc').val(titleTc);
 		$('#contentEn').val(contentEn);
 		$('#contentTc').val(contentTc);
 		$('#selectMessageType').find('option[value='+ messageType +']').attr('selected', 'selected');		
+		$('#selectURLType').find('option[value='+ apiUrl +']').attr('selected', 'selected');		
 		
 		connection.trigger('updateButton', { button: 'next', enabled: false });
 		gotoStep(step);
@@ -105,6 +108,11 @@ define( function( require ) {
         }
     };
 
+	function getApiUrl()
+	{
+		return $('input:radio[name=apiUrl]').filter(":checked").val();
+	};
+	
     function getTitleEn()
 	{
 		var str = $('#titleEn').val();
@@ -142,19 +150,21 @@ define( function( require ) {
     };
 
     function save() {
-
+		var apiUrl = getApiUrl();
         var titleEn = getTitleEn();
 		var contentEn = getContentEn();
 		var titleTc = getTitleTc();
 		var contentTc = getContentTc();
 		var messageType = getMessageType();
 		
+        toJbPayload['arguments'].execute.inArguments.push({"apiUrl": apiUrl});
         toJbPayload['arguments'].execute.inArguments.push({"titleEn": titleEn});
 		toJbPayload['arguments'].execute.inArguments.push({"contentEn": contentEn});
 		toJbPayload['arguments'].execute.inArguments.push({"titleTc": titleTc});
 		toJbPayload['arguments'].execute.inArguments.push({"contentTc": contentTc});
 		toJbPayload['arguments'].execute.inArguments.push({"messageType": messageType});
 		
+		toJbPayload['configurationArguments'].apiUrl = apiUrl;
 		toJbPayload['configurationArguments'].titleEn = titleEn;
 		toJbPayload['configurationArguments'].titleTc = titleTc;
 		toJbPayload['configurationArguments'].contentEn = contentEn;
